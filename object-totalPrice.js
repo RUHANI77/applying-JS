a9cb3acc8a7f571afe91b5aef0e3b67853882b99
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-06 12:03:39 +0600

    object total price

---
+++ object-totalPrice.js
@@ -0,0 +1,20 @@
+let productOne = {
+    name: "Laptop",
+    color: "black",
+    price: 12000
+};
+
+let productTwo = {
+    name: "Monitor",
+    color: "gray",
+    price: 5000
+};
+
+let productThree = {
+    name: "Mobile",
+    color: "black",
+    price: 2500
+}
+
+let result = (productOne.price + productTwo.price + productThree.price);
+console.log(result);
