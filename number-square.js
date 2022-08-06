7a252797575e302b29d17480ff443c71e874152d
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-06 15:20:17 +0600

    Square of a number

---
+++ number-square.js
@@ -0,0 +1,10 @@
+/*
+Write a function which will take an integer and will return the square of a 
+number
+*/
+function square(a) {
+    let result = a * a;
+    return 'Square of ' + a + ' is ' + result;
+}
+let n = 6;
+console.log(square(n));
