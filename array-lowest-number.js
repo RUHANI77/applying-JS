e7d22f9b8c297c4358c479e02f8c49afa50b512e
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-07 22:37:10 +0600

    Lowest number of an array

--- array-lowest-number.js
+++ array-lowest-number.js
@@ -1,2 +1,16 @@
 //Find the lowest Number of an Array
 
+function ArrayLowestNumber(a) {
+    let lowest = a[0];
+    for (let i = 0; i < a.length; i++){
+        let element = a[i];
+        if (element < lowest) {
+            lowest = element;
+        }
+    }
+    return lowest;
+}
+
+let a = [2, 4, 5, 6, 7, 8, 9, 45, 67, 1, 99];
+const lowestNumber = ArrayLowestNumber(a);
+console.log(lowestNumber);
