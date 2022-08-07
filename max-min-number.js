d0d0150518a8c498cba6fefffa5c10a8240d53fa
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-07 22:31:04 +0600

    Max and Min number

--- max-min-number.js
+++ max-min-number.js
@@ -1,6 +1,25 @@
 //Write a function that will take 3 parameters and will return the max number.
 
+function maxNumber(a, b, c) {
+    let maxNum = Math.max(a, b, c);
+    return maxNum;
+}
+
+let num1 = 2;
+let num2 = 6;
+let num3 = 11;
+
+const result = maxNumber(num1, num2, num3);
+console.log(result);
+
 //Write a function that will take 3 parameters and will return the min number.
+
+function minNumber(a, b, c) {
+    let minNum = Math.min(a, b, c);
+    return minNum;
+}
+const minNum = minNumber(num1, num2, num3);
+console.log(minNum); 
 
 
 
