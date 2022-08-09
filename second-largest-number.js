3368982c72fb26952e1115bd1ae4ecff359c9e7e
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-09 17:40:01 +0600

    Second largest number

--- second-largest-number.js
+++ second-largest-number.js
@@ -1,2 +1,22 @@
 //কোন একটা array এর মধ্যে অনেকগুলা সংখ্যা আছে। সেই সংখ্যাগুলো থেকে second largest সংখ্যা বের করার একটা প্রোগ্রাম লিখো।
 
+const arr = [3, 66, 7, 34, 56, 12, 33, 45, 99, 88, 100,145];
+
+function secondLargestNumber(arr) {
+    let largest = arr[0];
+    let secondLargest = arr[0];
+   for (let i = 0; i < arr.length; i++){
+        const element = arr[i];
+        if (largest < element) {
+            secondLargest = largest;
+            largest = arr[i];
+       }
+        else if (arr[i] > secondLargest) {
+            secondLargest = arr[i];
+       }
+    }
+    
+    console.log('First Largest Number: ',largest);
+    console.log('Second Largest Number: ',secondLargest);
+}
+console.log(secondLargestNumber(arr));
